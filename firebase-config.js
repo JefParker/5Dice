1a81570f9408234c2bdb9e13c14ae01fc129f40f
Jeff Parker <Jeff@Jeff-Parker.com>
2026-07-18 21:31:33 -0700

    Include restricted firebase config file in deployment

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBacG4ZE3ZPJ9VSm5-XAGktt4QD9LiNPvs",
+  authDomain: "dice-score.firebaseapp.com",
+  databaseURL: "https://dice-score-default-rtdb.firebaseio.com",
+  projectId: "dice-score",
+  storageBucket: "dice-score.firebasestorage.app",
+  messagingSenderId: "291526558646",
+  appId: "1:291526558646:web:2f9cd5755f4fae1c503717",
+  measurementId: "G-08RNMPNBHY"
+};
